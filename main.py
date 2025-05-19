ed0447b899154b5db6d3b548a96de7a80aa97d76
Isa Dev <isabella.salcedo.engineer@gmail.com>
2025-05-19 12:52:02 -0500

    Adicion de colisiones y bloques de salto

--- main.py
+++ main.py
@@ -24,8 +24,29 @@ FPS = 60
 # Cargar imagen del personaje
 fox_image = pygame.image.load("zorroAventurero.png").convert_alpha()
 fox_image = pygame.transform.scale(fox_image, (200, 200))  # ajusta el tamaño del personaje
-fox_rect = fox_image.get_rect()
-fox_rect.topleft = (100, 400)  # Posición inicial
+fox_rectemp = fox_image.get_rect().inflate(-110,0)
+fox_rect = fox_rectemp.copy()
+fox_rect.x = fox_rect.x + 50
+
+
+# Cargar bloques continuos del juego
+blockC = pygame.image.load("bloqueContinuo.png").convert_alpha()
+blockC = pygame.transform.scale(blockC, (300,150))
+
+
+# Lista de muros (puedes tener muchas)
+walls = [
+    pygame.Rect(100, 500, 300, 50),
+    pygame.Rect(350, 500, 300, 50),
+    pygame.Rect(600, 500, 300, 50),
+]
+
+
+#Cargar bloques flotantes
+block_1 = pygame.image.load("bloque1.png").convert_alpha()
+block_1 = pygame.transform.scale(block_1, (200,200))
+block1_rect = block_1.get_rect()
+block1_rect.topleft = (10, 400)  # Posición inicial
 
 
 # Variables de movimiento
@@ -49,15 +70,17 @@ def main():
 
         # Movimiento con teclas
         if keys[pygame.K_LEFT]:
-            fox_rect.x -= vel_x
-            for i in range(len(bg_positions)):
+            fox_rect.x -= vel_x  #movimiento zorro
+            fox_rectemp.x -= vel_x #movimiento rect colision
+            for i in range(len(bg_positions)): #movimiento fondo
                 bg_positions[i] += vel_x
 
         if keys[pygame.K_RIGHT]:
-            fox_rect.x += vel_x
+            fox_rect.x += vel_x 
+            fox_rectemp.x += vel_x
             for i in range(len(bg_positions)):
                 bg_positions[i] -= vel_x
-            
+
 
         #Salto    
         if keys[pygame.K_UP] and on_ground:
@@ -67,12 +90,20 @@ def main():
         # Aplicar gravedad
         vel_y += gravity
         fox_rect.y += vel_y
+        fox_rectemp.y += vel_y
 
-        # Colisión con el suelo
-        if fox_rect.bottom >= 500:
-            fox_rect.bottom = 500
-            vel_y = 0
-            on_ground = True
+
+        # Colisiones con bloques (suelo)
+        for i, wall in enumerate(walls):
+            wall_screen_x = wall.x + bg_positions[i % len(bg_positions)]
+            wall_moved = wall.move(bg_positions[i % len(bg_positions)], 0)
+
+            if fox_rect.colliderect(wall_moved):
+                if vel_y > 0 and fox_rect.bottom <= wall_moved.bottom:
+                    fox_rect.bottom = wall_moved.top
+                    fox_rectemp.bottom = wall_moved.top
+                    vel_y = 0
+                    on_ground = True
 
         #colisiones laterales para zorro y fondo
         if fox_rect.right >= 600:
@@ -81,6 +112,7 @@ def main():
         if fox_rect.left <= 50:
             fox_rect.left = 50
 
+
         # Reposicionar fondos cuando salen de pantalla
         for i in range(len(bg_positions)):
             if bg_positions[i] <= -bg_width:
@@ -88,12 +120,22 @@ def main():
             elif bg_positions[i] >= bg_width * (len(bg_positions) - 1):
                 bg_positions[i] -= bg_width * len(bg_positions)
 
+
         # Dibujar fondos
         for i, bg in enumerate(backgrounds):
             screen.blit(bg, (bg_positions[i], 0))
+            
+            
+        # Dibujar muros con su desplazamiento
+        for i, wall in enumerate(walls):
+            wall_screen_x = wall.x + bg_positions[i % len(bg_positions)]
+            #pygame.draw.rect(screen, (0, 0, 0), (wall_screen_x, wall.y, wall.width, wall.height))
+            screen.blit(blockC, (wall_screen_x, wall.y))
 
-        # Aqui inicia la logica del juego
-        screen.blit(fox_image, fox_rect)
+        # Dibujar personaje 
+
+        screen.blit(fox_image, fox_rectemp)
+        pygame.draw.rect(screen, (0, 255, 255), fox_rect, 2)
 
 
         pygame.display.flip()
